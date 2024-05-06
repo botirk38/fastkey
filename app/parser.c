3bd8c8396a826a06abc3a919822dedb6a14709a0
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-05-06 22:42:00 +0100

    codecrafters submit [skip ci]

--- app/parser.c
+++ app/parser.c
@@ -145,7 +145,7 @@ void parse_key_value(FILE *rdb_file, KeyValueStore *store) {
   }
 
   default:
-    printf("Unsupported value type %c\n", value_type);
+    printf("Unsupported value type %02X\n", value_type);
     exit(EXIT_FAILURE);
   }
 }
