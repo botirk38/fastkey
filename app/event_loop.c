0536116d0f49492bafc66fc87f0e7b9e04f00c73
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2025-01-25 15:22:45 +0000

    Feat: Use kqueue on apple systems

--- app/event_loop.c
+++ app/event_loop.c
@@ -1,13 +1,75 @@
 #include "event_loop.h"
 #include "client_handler.h"
 #include <stdlib.h>
-#include <sys/epoll.h>
 #include "networking.h"
+
+#ifdef __linux__
+static int create_event_loop_fd(void) {
+    return epoll_create1(0);
+}
+
+static void* create_events_array(void) {
+    return malloc(sizeof(struct epoll_event) * MAX_EVENTS);
+}
+
+static void add_fd(EventLoop* loop, int fd) {
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = fd;
+    epoll_ctl(loop->epollFd, EPOLL_CTL_ADD, fd, &ev);
+}
+
+static void remove_fd(EventLoop* loop, int fd) {
+    epoll_ctl(loop->epollFd, EPOLL_CTL_DEL, fd, NULL);
+}
+
+static int wait_for_events(EventLoop* loop) {
+    return epoll_wait(loop->epollFd, loop->events, MAX_EVENTS, -1);
+}
+
+static int get_fd_from_event(EventLoop* loop, int index) {
+    return loop->events[index].data.fd;
+}
+#elif defined(__APPLE__)
+static int create_event_loop_fd(void) {
+    return kqueue();
+}
+
+static void* create_events_array(void) {
+    return malloc(sizeof(struct kevent) * MAX_EVENTS);
+}
+
+static void add_fd(EventLoop* loop, int fd) {
+    struct kevent ev;
+    EV_SET(&ev, fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+    kevent(loop->kqueueFd, &ev, 1, NULL, 0, NULL);
+}
+
+static void remove_fd(EventLoop* loop, int fd) {
+    struct kevent ev;
+    EV_SET(&ev, fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+    kevent(loop->kqueueFd, &ev, 1, NULL, 0, NULL);
+}
+
+static int wait_for_events(EventLoop* loop) {
+    return kevent(loop->kqueueFd, NULL, 0, loop->events, MAX_EVENTS, NULL);
+}
+
+static int get_fd_from_event(EventLoop* loop, int index) {
+    return (int)loop->events[index].ident;
+}
+#endif
 
 EventLoop* createEventLoop(void) {
     EventLoop* loop = malloc(sizeof(EventLoop));
-    loop->epollFd = epoll_create1(0);
-    loop->events = malloc(sizeof(struct epoll_event) * MAX_EVENTS);
+    
+    #ifdef __linux__
+    loop->epollFd = create_event_loop_fd();
+    #elif defined(__APPLE__)
+    loop->kqueueFd = create_event_loop_fd();
+    #endif
+    
+    loop->events = create_events_array();
     loop->running = 1;
     loop->clientCount = 0;
     
@@ -18,24 +80,21 @@ EventLoop* createEventLoop(void) {
 }
 
 void eventLoopAddFd(EventLoop* loop, int fd, int events) {
-    struct epoll_event ev;
-    ev.events = events;
-    ev.data.fd = fd;
-    epoll_ctl(loop->epollFd, EPOLL_CTL_ADD, fd, &ev);
+    add_fd(loop, fd);
 }
 
 void eventLoopRemoveFd(EventLoop* loop, int fd) {
-    epoll_ctl(loop->epollFd, EPOLL_CTL_DEL, fd, NULL);
+    remove_fd(loop, fd);
 }
 
 void eventLoopStart(EventLoop* loop, RedisServer* server) {
-    eventLoopAddFd(loop, server->fd, EPOLLIN);
+    add_fd(loop, server->fd);
 
     while (loop->running) {
-        int nfds = epoll_wait(loop->epollFd, loop->events, MAX_EVENTS, -1);
+        int nfds = wait_for_events(loop);
         
         for (int i = 0; i < nfds; i++) {
-            int fd = loop->events[i].data.fd;
+            int fd = get_fd_from_event(loop, i);
             
             if (fd == server->fd) {
                 int clientFd = acceptClient(server);
--- app/event_loop.h
+++ app/event_loop.h
@@ -5,7 +5,12 @@
 #include "resp.h"
 #include "server.h"
 #include <stdlib.h>
+
+#ifdef __linux__
 #include <sys/epoll.h>
+#elif defined(__APPLE__)
+#include <sys/event.h>
+#endif
 
 #define MAX_EVENTS 1024
 #define MAX_CLIENTS 1024
@@ -15,16 +20,19 @@ typedef struct {
   RespBuffer *buffer;
   int in_transaction;
   CommandQueue *queue;
-
 } ClientState;
 
 typedef struct {
+#ifdef __linux__
   int epollFd;
   struct epoll_event *events;
+#elif defined(__APPLE__)
+  int kqueueFd;
+  struct kevent *events;
+#endif
   int running;
   ClientState *clients[MAX_EVENTS];
   size_t clientCount;
-
 } EventLoop;
 
 EventLoop *createEventLoop(void);
@@ -33,3 +41,4 @@ void eventLoopRemoveFd(EventLoop *loop, int fd);
 void eventLoopStart(EventLoop *loop, RedisServer *server);
 
 #endif
+
